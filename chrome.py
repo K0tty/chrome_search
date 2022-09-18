218c92c4d0f343681fbd670eaf8694aed4b7c6ee
K0tty <king_surge37@gmail.com>
2022-09-18 18:53:41 +0900

    2 commit

---
+++ chrome.py
@@ -0,0 +1,22 @@
+# モジュール読み込み
+import requests
+from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# WEBブラウザの起動とChromedriverとChromeのバージョン調整
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# 特定のURLへ移動
+driver.get('http://www.google.com/') 
+time.sleep(2)
+
+# ページ上の要素の取得
+search_box = driver.find_element(By.NAME, 'q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+
+with open('index.html', 'w', encoding='utf-8') as f:
+  f.write(res.text)
